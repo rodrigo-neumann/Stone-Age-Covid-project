8c5d32be51381aaffceb89015b46ed3b0b687bcc
rodrigo-neumann <53343376+rodrigo-neumann@users.noreply.github.com>
2021-02-10 20:45:03 -0300

    Add extraction of state data

--- data_extraction.R
+++ data_extraction.R
@@ -7,8 +7,18 @@ require(dplyr)
 require(httr)
 require(readr)
 
-
+# Exrtract National data from API
 url_call="https://api.covidtracking.com/v1/us/daily.csv"
 
 raw_data <- GET(url_call,body=NULL,encode = "json")
 data_set_raw<-httr::content(raw_data)
+
+write.csv(data_set_US_raw,"./Stone Age Covid project/data/raw_national_data.csv")
+
+# Exrtract State data from API
+url_call="https://api.covidtracking.com/v1/states/daily.csv"
+
+raw_data <- GET(url_call,body=NULL,encode = "json")
+data_set_state_raw<-httr::content(raw_data)
+
+write.csv(data_set_state_raw,"./Stone Age Covid project/data/raw_state_data.csv")
