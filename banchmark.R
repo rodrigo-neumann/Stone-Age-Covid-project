81cda1627f0bce6a595e819ac45d5b79076e8330
rodrigo-neumann <53343376+rodrigo-neumann@users.noreply.github.com>
2021-02-11 21:10:06 -0300

    add visualization for benchmark

--- banchmark.R
+++ banchmark.R
@@ -3,19 +3,25 @@
 
 require(dplyr)
 require("forecast")
+require(ggplot2)
 
 load("./Stone Age Covid project/data/data_set_US.rdata")
 
 #primiero passo bencchamark: Naive and Snaive 
 
-naive=naive(y=US_train$deathIncrease,data=US ,h=30)
+naive=naive(y=US_train$deathIncrease,h=30)
 naive_precdictions=naive$mean
 
-US_train$date_p %>% max()
+snaive=snaive(y=US_train$deathIncrease,h=30)
+snaive_precdictions=snaive$mean
 
-US_train$
+
+predicted_timse=seq(as.Date("2020-11-01"), as.Date("2020-11-30"), by = "days")
+
+
 
 ggplot() +
-  geom_line(aes(x=US$date_p, y=US$deathIncrease))+
-  geom_line(aes(x=US$date_p, y=US$deathIncrease))
+  geom_line(aes(x=US_train$date_p, y=US_train$deathIncrease,color="Train"))+
+  geom_line(aes(x=predicted_timse, y=naive_precdictions,color="Naive_ predict"))+
+  geom_line(aes(x=predicted_timse, y=snaive_precdictions,color="Snaive_ predict"))
 
